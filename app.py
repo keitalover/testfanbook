37c5837c9eb1e91fea1d6725771858803ddb71b6
jihantriananda <jihantri727@gmail.com>
2023-06-03 20:59:33 +0700

    test

--- app.py
+++ app.py
@@ -69,4 +69,4 @@ def homework_get():
     return jsonify({'message':message_list})
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=5000, debug=True
+    app.run('0.0.0.0', port=5000, debug=True)
